43b8ce91908710c11f68f9cf3734a8980ef28280
Scheib Ákos <schaki9518@gmail.com>
2023-06-19 20:19:03 +0200

    Element test update

--- tests/test_add_element_button.py
+++ tests/test_add_element_button.py
@@ -13,7 +13,7 @@ class TestAddElementButton(object):
     def setup_method(self):
         service = Service(executable_path=ChromeDriverManager().install())
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_experimental_option("detach", True)
         self.browser = webdriver.Chrome(service=service, options=options)
         URL = "http://the-internet.herokuapp.com/add_remove_elements/"
@@ -26,10 +26,10 @@ class TestAddElementButton(object):
     @allure.title("Add and remove element button test")
     def test_adding_elements(self):
         wait = WebDriverWait(self.browser, 5)
-        add_element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='example']/button[@onclick='addElement()']")))
+        add_element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='example']/button")))
         clicks = 5
         for i in range(clicks):
-            add_element.click
+            add_element.click()
         element_list = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@id='elements']/button[@class='added-manually']")))
-        print(f"The test was run by clicking the add element button {clicks} times.\nThe number of visible new buttons after the clicks: {element_list.size()}")
-        assert element_list.size() == clicks
+        print(f"The test was run by clicking the add element button {clicks} times.\nThe number of visible new buttons after the clicks: {len(element_list)}")
+        assert len(element_list) == clicks
